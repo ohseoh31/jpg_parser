2837d03af6492912b52dd359baadf18066ea7b72
ohseoh31 <ohseoh44@naver.com>
2019-02-11 15:32:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+################################################################################
+# Copyright (Python) taegyu <https://github.com/ohseoh31>                      #
+# @author taegue (ohseoh31@github.com) bob7 df                                 #
+# @brief  [jpg, jpeg]file marker parser code with python                       #
+# using command : python -i [filename]                                         #
+################################################################################
+
 from option import Option
 
 
